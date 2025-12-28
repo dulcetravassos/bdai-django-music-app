1eb5b1ccac8a04727705ed4736410b5ac9c14d0d
Dulce Travassos <158324644+dulcetravassos@users.noreply.github.com>
2025-12-28 19:19:05 +0000

    Created admin.py and updated models

--- bdai_django_music_app/biblioteca/admin.py
+++ bdai_django_music_app/biblioteca/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import MusicaEstatistica, Artista, Album, Playlist, Utilizador
 
-# Register your models here.
+@admin.register(Artista)
+class ArtistaAdmin(admin.ModelAdmin):
+	list_display = ('nome', 'biografia')
+	search_fields = ('nome',)
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+	list_display = ('nome', 'ano_lancamento', 'genero', 'artista')
+	list_filter = ('artista',)
+	search_fields = ('nome', 'ano_lancamento', 'artista__nome') # artista__nome - procura pelo nome do artista
+
+@admin.register(Utilizador)
+class UtilizadorAdmin(admin.ModelAdmin):
+	list_display = ('nome_utilizador', 'email')
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+	list_display = ('nome', 'publica', 'data_criacao', 'utilizador', 'get_musicas')
+	list_filter = ('publica', 'data_criacao',)
+	
+	filter_horizontal = ('musica',) # widget visual
+    
+	def get_musicas(self, obj): # para permitir mostrar campos ManyToMany diretamente no display (uma playlist pode ter várias músicas...)
+		return ", ".join([m.nome for m in obj.musica.all()]) # transforma a lista numa string
+	get_musicas.short_description = 'Músicas'
+
+@admin.register(MusicaEstatistica)
+class MusicaEstatisticaAdmin(admin.ModelAdmin):
+	list_display = ('nome', 'duracao', 'url', 'estatistica_visualizacoes', 'estatistica_gostos', 'album', 'get_artistas')
+	list_filter = ('album',)
+	search_fields = ('nome',)
+
+	filter_horizontal = ('artistas',)
+
+	def get_artistas(self, obj): # para permitir mostrar campos ManyToMany diretamente no display (uma música pode ter vários artistas...)
+		return ", ".join([a.nome for a in obj.artistas.all()])
+	get_artistas.short_description = 'Artistas'
--- bdai_django_music_app/biblioteca/models.py
+++ bdai_django_music_app/biblioteca/models.py
@@ -4,6 +4,10 @@ class Artista(models.Model):
     nome = models.CharField(max_length=512)
     biografia = models.CharField(max_length=512, blank=True, null=True)
 
+    class Meta:
+        verbose_name = 'Artista'
+        verbose_name_plural = 'Artistas' # Override standard que é o inglês
+
     def __str__(self):
         return f"{self.nome}\nBiografia: {self.biografia}\n"
 
@@ -12,6 +16,10 @@ class Utilizador(models.Model):
     nome_utilizador = models.CharField(max_length=512)
     email = models.CharField(max_length=512, unique=True)
     password = models.CharField(max_length=512)
+
+    class Meta:
+        verbose_name = 'Utilizador'
+        verbose_name_plural = 'Utilizadores' # Override standard que é o inglês
 
     def __str__(self):
         return self.nome_utilizador
@@ -22,6 +30,10 @@ class Album(models.Model):
     ano_lancamento = models.BigIntegerField()
     genero = models.CharField(max_length=512, null=True, blank=True) # opcional
     artista = models.ForeignKey(Artista, on_delete=models.CASCADE, related_name='albuns')
+
+    class Meta:
+        verbose_name = 'Álbum'
+        verbose_name_plural = 'Álbuns' # Override standard que é o inglês
 
     def __str__(self):
         return f"{self.nome}, {self.artista.nome} ({self.ano_lancamento})"
@@ -38,6 +50,10 @@ class MusicaEstatistica(models.Model):
     # Relação N:M com a tabela Artista
     artistas = models.ManyToManyField(Artista, related_name='musicas_estatisticas', db_table='artista_musica_estatistica') # Correspondência com a tabela na bd
 
+    class Meta:
+        verbose_name = 'Música'
+        verbose_name_plural = 'Músicas' # Override standard que é o inglês
+
     def __str__(self):
         return f"{self.nome} ({self.album.nome}) - {self.url}"
     
@@ -50,6 +66,10 @@ class Playlist(models.Model):
 
     # Relação M:N com a tabela Musica:
     musica = models.ManyToManyField(MusicaEstatistica, blank=True, related_name='musicas_estatisticas', db_table='playlist_musica_estatistica') # Correspondência com a tabela na bd
+
+    class Meta:
+        verbose_name = 'Playlist'
+        verbose_name_plural = 'Playlists' # Override standard que é o inglês
 
     def __str__(self):
         return f"{self.nome}"
